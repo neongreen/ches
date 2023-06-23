fff425a1153c93640b680b1a61a8d66358878198
Artyom Kazak <artyom@artyom.me>
2023-06-24 00:04:31 +0200

    Add move numbers to sample games

--- tools/sample-games.ts
+++ tools/sample-games.ts
@@ -11,8 +11,10 @@ import { notateMove } from '@/move'
 import { writeFileSync } from 'fs'
 import path from 'path'
 import { renderScore, Score } from '@/eval/score'
+import { Color } from '@/piece'
 
 for (let depth = 1; depth <= 5; depth++) {
+  console.debug(`Generating a sample game for depth ${depth}`)
   const filename = path.resolve(__dirname, `sample-games/depth-${depth}.txt`)
   const board = new Board()
   const game: [Score, string][] = []
@@ -23,7 +25,12 @@ for (let depth = 1; depth <= 5; depth++) {
       game.push([best.score, best.score === 0 ? '1/2-1/2' : best.score > 0 ? '1-0' : '0-1'])
       break
     } else {
-      game.push([best.score, notateMove(board, best.move)])
+      const moveNumber = Math.floor(game.length / 2) + 1
+      const notation =
+        board.side === Color.White
+          ? `${moveNumber}. ${notateMove(board, best.move)}`
+          : `... ${notateMove(board, best.move)}`
+      game.push([best.score, notation])
       board.executeMove(best.move)
     }
   }
