429de1edf54db953d9f67f042ff3fd0944da39a0
Artyom Kazak <artyom@artyom.me>
2023-06-25 16:43:06 +0200

    Icon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,15 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/react'
 import { MantineProvider } from '@mantine/core'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <link rel="icon" href="/favicon.png" />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
       <MantineProvider
         withGlobalStyles
         withNormalizeCSS
