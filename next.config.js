9bb79073639d4b844a4be0d34f6f16fa8921789f
Artyom Kazak <artyom@artyom.me>
2023-06-26 18:59:47 +0200

    Repro

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // https://github.com/P5-wrapper/react/issues/255
-  reactStrictMode: false,
-}
-
-module.exports = nextConfig
--- src/challenges/all.ts
+++
@@ -1,8 +0,0 @@
-import { chessSimpChallenges } from './chess-simp'
-import { chessSimpDiscordChallenges } from './chess-simp-discord'
-import { Challenge } from './core'
-
-export const challenges: { group: string; list: Challenge[] }[] = [
-  { group: 'Chess Simp', list: chessSimpChallenges },
-  { group: 'Chess Simp Discord', list: chessSimpDiscordChallenges },
-]
